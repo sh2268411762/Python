ee55c10d6cc77ce47a663b4edebfe5693282e764
sh2268411762 <2268411762@qq.com>
2020-04-21 00:35:12 +0800

    Python

---
+++ 流程控制（Python）/流程控制_Python_.py
@@ -0,0 +1,74 @@
+##!/usr/bin/env python3
+## _*_ coding:utf-8 _*_
+
+#x = True
+#if x:
+#    print("It's True")
+
+#x = False
+#if x:
+#    print("It's False")
+##无法输出，x为False，无法执行if语句
+
+#x = 18
+#if x:
+#    print("x is",x)
+
+#x = 0
+#if x:
+#    print("x is",x)
+##不输出
+
+
+#x = "HELLo"
+#if x:
+#    print("x is",x)
+
+#x = ""
+#if x:
+#    print("x is")
+##不输出
+
+#x1 = []
+#x2 = [1,2]
+#if x1:
+#    print("x1")#不输出
+
+#if x2:
+#    print("x2")
+
+
+#x3 =()
+#x4 =(15,3)
+#if x3:
+#    print("x3")#不输出
+
+#if x4:
+#    print("x4")
+
+
+#x5 = {}
+#x6 = {"HELLO","二球"}
+#if x5:
+#    print("x5")
+
+#if x6:
+#    print("x6")
+
+
+x = 0
+if x:
+    print("x不是0")
+else:
+    print("x是0")
+
+
+x = 49
+if x>90:
+    print("优")
+elif x>80:
+    print("良")
+elif x>60:
+    print("及格")
+else:
+    print("不及格")
